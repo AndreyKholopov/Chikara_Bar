37060f4b8fb2e2878299633b4318a9223e362da9
AndreyKholopov <Andrey.Kholopov.Work@gmail.com>
2020-01-06 18:13:47 +0300

    add rotation menu button

---
+++ js/main.js
@@ -0,0 +1,31 @@
+// Select DOM Items
+const menuBtn = document.querySelector('.menu-btn');
+const menu = document.querySelector('.menu');
+const menuNav = document.querySelector('.menu-nav');
+const menuLogo = document.querySelector('.menu-logo');
+const navItems = document.querySelectorAll('nac-item');
+
+//Set Initial State of Menu
+let showMenu = false;
+
+menuBtn.addEventListener('click', toggleMenu);
+
+function toggleMenu() {
+	if(!showMenu) {
+		menuBtn.classList.add('close');
+		menu.classList.add('Show');
+		menuNav.classList.add('Show');
+		menuLogo.classList.add('Show');
+		navItems.forEach(item => item.classList.add ('Show'));
+
+		showMenu = true;
+	} else {
+		menuBtn.classList.remove('close');
+		menu.classList.remove('Show');
+		menuNav.classList.remove('Show');
+		menuLogo.classList.remove('Show');
+		navItems.forEach(item => item.classList.remove ('Show'));
+
+		showMenu = false;
+	}
+}
